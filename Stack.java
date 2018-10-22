5bd281b4b4c570a8aa297d3200a37d71ab33a59c
Peterzhu123 <Peter.zhu@ocsbstudent.ca>
2018-10-21 22:23:18 -0400

    Create Stack.java

---
+++ Stack.java
@@ -0,0 +1,35 @@
+/****************************************************************************
+ *
+ * Created by: Peter Zhu
+ * Created on: Oct
+ * This is a program about stack which import MrCoxallClass
+ *
+ ****************************************************************************/
+
+import java.util.*;
+
+public class Stack {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter an Integer that can put on the stack. ");
+		Scanner scanner = new Scanner(System.in);
+		int user = scanner.nextInt();
+		MrCoxallStack stack = new MrCoxallStack();
+		
+		// Push the number on the array
+		stack.push(user);
+		stack.push(10);
+		stack.push(500);
+		
+		// Pop the number out of the array
+		stack.pop(user);
+		
+		// print the array list
+		stack.print();
+		
+		// Count how many numbers in the array list
+		stack.count();
+
+	}
+	
+}
